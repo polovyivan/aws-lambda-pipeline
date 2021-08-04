b32c2376df6d011e6c4e9bc051166b20a3d0101f
Ivan Polovyi <ivan.polovyi@sensedia.com>
2021-08-04 09:56:25 -0300

    init commit

---
+++ src/main/java/com/ivan/polovyi/examples/SQSHandler.java
@@ -0,0 +1,21 @@
+package com.ivan.polovyi.examples;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+
+
+public class SQSHandler implements RequestHandler<SQSEvent, Void> {
+
+    @Override
+    public Void handleRequest(SQSEvent sqsEvent, Context context) {
+        context.getLogger().log("\nFunction started. Processing " + sqsEvent.getRecords().size() + " messages\n");
+
+        sqsEvent.getRecords().forEach(message -> {
+            context.getLogger().log("Message id " + message.getMessageId() + "\n");
+            context.getLogger().log("Message body " + message.getBody()+ "\n");
+
+        });
+        return null;
+    }
+}
